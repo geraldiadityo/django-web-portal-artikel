4582b9449edf8d2f7535dc01d926c97e1ce7c891
geraldi adityo <geraldi19adityo@gmail.com>
2021-01-10 10:25:31 +0700

    regiter router url peraplikasi ke root url

--- webartikelajax/urls.py
+++ webartikelajax/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
+    path('',include('artikel.urls',namespace='artikel')),
+    path('penulis/',include('penulis.urls',namespace='penulis')),
     path('admin/', admin.site.urls),
 ]
